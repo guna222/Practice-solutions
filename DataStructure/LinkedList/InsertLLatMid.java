aba0123b7771576801af552b9f254f71bfe75f09
guna222 <62829533+guna222@users.noreply.github.com>
2020-03-29 17:08:36 +0530

    Create InsertLLatMid.java

---
+++ DataStructure/LinkedList/InsertLLatMid.java
@@ -0,0 +1,56 @@
+public class InsertLLatMid
+{
+
+    static Node head;
+
+    static class Node
+    {
+        int data;
+        Node next;
+
+        Node(int data)
+        {
+            this.data = data;
+        }
+    }
+
+    public static void main(String[] args)
+    {
+        head = new Node(10);
+        head.next = new Node(20);
+        head.next.next = new Node(12);
+        head.next.next.next = new Node(34);
+        head.next.next.next.next = new Node(44);
+        head.next.next.next.next.next = new Node(16);
+
+        Node temp = head;
+        int count = 0;
+        while (head != null)
+        {
+            head = head.next;
+            count++;
+        }
+        int midoflinklist = count / 2;
+        //int midoflinklist = ((count % 2) == 0) ? (count / 2) : (count + 1) / 2;
+        int loop = 1;
+        System.out.println("midoflinklist   " + midoflinklist);
+
+        while (temp != null)
+        {
+            if (loop == midoflinklist)
+            {
+                Node newNode = new Node(99);
+                newNode.next = temp.next;
+                temp.next = newNode;
+
+            }
+
+            System.out.println(temp.data);
+            temp = temp.next;
+            loop++;
+
+        }
+
+    }
+
+}
